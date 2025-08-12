b70a7438557b91ef945a167c584ef24fb10303b4
Aditya <adiii202004@gmail.com>
2025-08-13 01:31:17 +0530

    Fix route handling to serve index.html with path parameters in production

--- server/src/index.js
+++ server/src/index.js
@@ -15,9 +15,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(_dirname, "../../client/dist")));
 
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(_dirname, "../../client", "dist", "index.html"))
-    })
+    app.get("/:path(*)", (req, res) => {
+        res.sendFile(path.join(_dirname, "../../client", "dist", "index.html"));
+    });
+
 }
 
 
